6e1945560c4992af30629bb3e302a4d809c0c256
deepakkhichar <deepakkhichar60@gmail.com>
2025-03-21 15:47:34 +0530

    Create admin user

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ pip3 install -r requirements.txt
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
 python3 manage.py scrape_hotels
+DJANGO_SUPERUSER_PASSWORD=Damin@321 python3 manage.py createsuperuser --noinput --username=admin1 --email=admin1@admin1.com
