18a209b6a2997ad047220d6b9eca643a682c85b0
deepakkhichar <deepakkhichar60@gmail.com>
2025-03-21 21:06:36 +0530

    [CHORE] Remove admin user creation through build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,3 @@ pip3 install -r requirements.txt
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
 python3 manage.py scrape_hotels
-DJANGO_SUPERUSER_PASSWORD=Damin@321 python3 manage.py createsuperuser --noinput --username=admin1 --email=admin1@admin1.com
